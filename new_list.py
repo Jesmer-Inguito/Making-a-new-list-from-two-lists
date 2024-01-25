9040626db07f0f2b235ca356b71b779973c904c5
unknown <you@example.com>
2024-01-25 13:52:53 +0800

    pseudocode

---
+++ new_list.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# Create a function that is responsible for making a new list
+
+# Create two lists
+
+# Create a variable that can be used to call the function
+
+# Print the 1st, 2nd and new lists
