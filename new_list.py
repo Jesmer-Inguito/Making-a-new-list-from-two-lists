5dfbae5ca4203ece8f99dff1af05469c53030753
unknown <you@example.com>
2024-01-25 13:55:31 +0800

    function done

--- new_list.py
+++ new_list.py
@@ -1,6 +1,12 @@
 # pseudocode
 
 # Create a function that is responsible for making a new list
+def create_new_list(list_1, list_2):
+    odd_numbers = [num for num in list_1 if num % 2 != 0]
+    even_numbers = [num for num in list_2 if num % 2 == 0]
+
+    result_list = odd_numbers + even_numbers
+    return result_list
 
 # Create two lists
 
